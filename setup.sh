112e9c20d5a3687409b6230bc5d59d44e3b7a86e
Dominik Schwabe <schwabedominik@gmail.com>
2018-12-23 22:45:22 +0100

    i3bar von i3status zu i3pystatus geändert

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-ln -s ~/config/.vimrc ~/.vimrc
-ln -s ~/config/.i3config ~/.config/i3/config
+ln -s ~/config/.vimrc ~/.vimrc $@
+ln -s ~/config/.i3config ~/.config/i3/config $@
+ln -s ~/config/.i3pystatus.conf ~/.config/i3/i3pystatus.conf $@
