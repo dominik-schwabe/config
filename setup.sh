c14aeaf7bfcbe3d74f146a023e23b43e7c076bf0
Dominik Schwabe <schwabedominik@gmail.com>
2019-03-10 10:13:18 +0100

    setup.sh legt Ordner an

--- setup.sh
+++ setup.sh
@@ -8,8 +8,10 @@ ln -s ~/config/.envrc ~/.envrc $@
 ln -s ~/config/.toprc ~/.toprc $@
 mkdir ~/.config/ranger -p
 ln -s ~/config/.rc.conf ~/.config/ranger/rc.conf $@
+mkdir ~/.config/termite -p
 ln -s ~/config/.termite.conf ~/.config/termite/config $@
 ln -s ~/config/.aliasrc ~/.aliasrc $@
+mkdir ~/.config/i3 -p
 ln -s ~/config/.i3config ~/.config/i3/config $@
 ln -s ~/config/.i3pystatus.conf ~/.config/i3/i3pystatus.conf $@
 ln -s ~/config/.Xdefaults ~/.Xdefaults $@
