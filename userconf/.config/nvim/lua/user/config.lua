4daa97d74174939a5ce1bc54db76861de52a5b19
Dominik Schwabe <schwabedominik@gmail.com>
2022-08-19 11:35:42 +0200

    pyright

--- userconf/.config/nvim/lua/user/config.lua
+++ userconf/.config/nvim/lua/user/config.lua
@@ -129,7 +129,7 @@ M.lsp_configs = {
       python = {
         analysis = {
           diagnosticMode = "openFilesOnly",
-          useLibraryCodeForTypes = true,
+          useLibraryCodeForTypes = false,
           typeCheckingMode = "off",
         },
       },
