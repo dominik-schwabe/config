b3cf572ad23b219aa3ba63c9f886460cda736187
Dominik Schwabe <schwabedominik@gmail.com>
2022-10-09 15:10:33 +0200

    fix comment

--- userconf/.config/nvim/lua/user/plugins/comment-nvim.lua
+++ userconf/.config/nvim/lua/user/plugins/comment-nvim.lua
@@ -6,13 +6,7 @@ require("Comment").setup({
   mappings = {
     basic = true,
     extra = true,
-    extended = true,
   },
 
   pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
 })
-
--- vim.keymap.set("n", "gc", "<Plug>(comment_toggle_current_linewise)")
--- vim.keymap.set("x", "gc", "<Plug>(comment_toggle_linewise_visual)")
--- vim.keymap.set("n", "gb", "<Plug>(comment_toggle_current_blockwise)")
--- vim.keymap.set("x", "gb", "<Plug>(comment_toggle_blockwise_visual)")
